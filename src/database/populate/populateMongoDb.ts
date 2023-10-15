7344a84cbd4e7bf5c2b117239f1e57017a64bbca
Filipe Barbosa <filipe.barbosa@gazin.com.br>
2023-10-15 15:08:54 -0300

    feat: [task-11] ajuste de documentação e arquivo de população

--- src/database/populate/populateMongoDb.ts
+++ src/database/populate/populateMongoDb.ts
@@ -1,27 +1,31 @@
-// import mongoose from 'mongoose';
-// import { schedulesData } from './dbData';
-// import { ScheduleModel } from './dbModels';
-//
-// const mongoDBUrl = 'mongodb://host.docker.internal:27017/local';
-//
-// async function populateDatabase() {
-//   try {
-//     await mongoose.connect(mongoDBUrl, {});
-//
-//     // const doctors = await DoctorModel.insertMany(doctorsData);
-//     // const units = await UnitModel.insertMany(unitsData);
-//     // const specialities = await SpecialityModel.insertMany(specialitiesData);
-//     const schedules = await ScheduleModel.insertMany(schedulesData);
-//
-//     // console.log('Dados doctors inseridos com sucesso.', doctors);
-//     // console.log('Dados units inseridos com sucesso.', units);
-//     // console.log('Dados specialities inseridos com sucesso.', specialities);
-//     console.log('Dados schedules inseridos com sucesso.', schedules);
-//   } catch (error) {
-//     console.error('Erro ao inserir dados:', error);
-//   } finally {
-//     await mongoose.disconnect();
-//   }
-// }
-//
-// populateDatabase();
+import mongoose from 'mongoose';
+import { DoctorModel, MinutesModel, ScheduleModel, SpecialityModel, UnitModel } from './dbModels';
+import { doctorsData, minutesData, schedulesData, specialitiesData, unitsData } from './dbData';
+
+const mongoDBUrl = 'mongodb://host.docker.internal:27017/local';
+
+async function populateDatabase() {
+  try {
+    await mongoose.connect(mongoDBUrl, {});
+
+    const minutes = await MinutesModel.insertMany(minutesData);
+    const doctors = await DoctorModel.insertMany(doctorsData);
+    const units = await UnitModel.insertMany(unitsData);
+    const specialities = await SpecialityModel.insertMany(specialitiesData);
+    const schedules = await ScheduleModel.insertMany(schedulesData);
+
+    console.log('Dados doctors inseridos com sucesso.', doctors);
+    console.log('Dados units inseridos com sucesso.', units);
+    console.log('Dados specialities inseridos com sucesso.', specialities);
+    console.log('Dados schedules inseridos com sucesso.', schedules);
+    console.log('Dados minutes inseridos com sucesso.', minutes);
+
+    console.log('\n\nDados populados com sucesso!!');
+  } catch (error) {
+    console.error('Erro ao inserir dados:', error);
+  } finally {
+    await mongoose.disconnect();
+  }
+}
+
+populateDatabase();
