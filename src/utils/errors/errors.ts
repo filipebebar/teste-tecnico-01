b9b38bc8caa8df51c2c229a0ce18451e20509b6d
Filipe Barbosa <filipe.barbosa@gazin.com.br>
2023-10-17 00:03:10 -0300

    fix: [task-14] ajustando retorno de erros

--- src/utils/errors/errors.ts
+++ src/utils/errors/errors.ts
@@ -1,21 +1,21 @@
 import { HttpStatus } from '@nestjs/common';
-
+//TOODO: ALTERAR O RETORNO DOS ERROS statusCode
 export const ERROS = {
   MONGO_DB: {
     UPDATE_EXCEPTION: {
       code: 1000,
       message: 'Erro no mongodb ao atualizar os dados!!',
-      statusCode: HttpStatus.NOT_FOUND,
+      statusCode: HttpStatus.BAD_REQUEST,
     },
     GET_LIST_EXCEPTION: {
       code: 1001,
       message: 'Erro no mongodb ao listar os dados!!',
-      statusCode: HttpStatus.NOT_FOUND,
+      statusCode: HttpStatus.BAD_REQUEST,
     },
     GET_ONE_EXCEPTION: {
-      code: 1001,
+      code: 1002,
       message: 'Erro no mongodb ao recuperar os dados!!',
-      statusCode: HttpStatus.NOT_FOUND,
+      statusCode: HttpStatus.BAD_REQUEST,
     },
   },
 
@@ -28,22 +28,22 @@ export const ERROS = {
     CANCEL_EXCEPTION: {
       code: 2001,
       message: 'Erro ao realizar o cancelamento do agendamento!',
-      statusCode: HttpStatus.NOT_FOUND,
+      statusCode: HttpStatus.BAD_REQUEST,
     },
     ALREADY_HAVE: {
       code: 2002,
       message: 'já existe um agendamento confirmado para está reserva!!',
-      statusCode: HttpStatus.NOT_FOUND,
+      statusCode: HttpStatus.AMBIGUOUS,
     },
     TIME_HAS_RUN_OUT: {
       code: 2003,
       message: 'O tempo para realizar o agendamento foi finalizado, por favor realize os passos novamente!',
-      statusCode: HttpStatus.NOT_FOUND,
+      statusCode: HttpStatus.ACCEPTED,
     },
     MORE_THAN_ONE_SAME_TIME: {
       code: 2004,
       message: 'Não é possível realizar mais de um agendamento seguidos!',
-      statusCode: HttpStatus.NOT_FOUND,
+      statusCode: HttpStatus.AMBIGUOUS,
     },
   },
 };
