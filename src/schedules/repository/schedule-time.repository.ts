2ed6c174392f89066b7ab9d49aaaa412e29d41b4
Filipe Barbosa <filipe.barbosa@gazin.com.br>
2023-10-17 01:49:06 -0300

    feat: [task-20] implementando repository e chamada e chamadas

---
+++ src/schedules/repository/schedule-time.repository.ts
@@ -0,0 +1,24 @@
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { ScheduleTime } from '../../database/models/Schedule-time.schema';
+
+export class ScheduleTimeRepository {
+  constructor(@InjectModel(ScheduleTime.name) private scheduleTimeModel: Model<ScheduleTime>) {}
+
+  async findOne() {
+    const result = await this.scheduleTimeModel.findOne();
+    return {
+      slotId: result.slotId,
+      minutes: result.minutes,
+    };
+  }
+
+  async updateBySlotId(scheduleTimeRequest) {
+    const { slotId, minutes } = scheduleTimeRequest;
+    const result = await this.scheduleTimeModel.updateOne({ slotId: slotId }, { $set: { minutes: minutes } });
+    if (result.modifiedCount === 1) {
+      return 'Tempo de reserva de agendamento atualizado com sucesso!';
+    }
+    return result;
+  }
+}
--- src/schedules/service/schedule-time.service.ts
+++ src/schedules/service/schedule-time.service.ts
@@ -1,28 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { ScheduleTime } from '../../database/models/Schedule-time.schema';
-import { DataBaseUpdateException } from '../exception/schedules.exception';
+import { DataBaseListException, DataBaseUpdateException } from '../exception/schedules.exception';
+import { ScheduleTimeRepository } from '../repository/schedule-time.repository';
 
 @Injectable()
 export class ScheduleTimeService {
-  constructor(@InjectModel(ScheduleTime.name) private scheduleTimeModel: Model<ScheduleTime>) {}
+  constructor(private readonly scheduleTimeRepository: ScheduleTimeRepository) {}
 
   async getMinutes() {
-    const result = await this.scheduleTimeModel.findOne();
-    return {
-      slotId: result.slotId,
-      minutes: result.minutes,
-    };
+    try {
+      return await this.scheduleTimeRepository.findOne();
+    } catch (e) {
+      throw new DataBaseListException();
+    }
   }
 
   async updateTimeSchedule(scheduleTimeRequest) {
-    const { slotId, minutes } = scheduleTimeRequest;
     try {
-      const result = await this.scheduleTimeModel.updateOne({ slotId: slotId }, { $set: { minutes: minutes } });
-      if (result.modifiedCount === 1) {
-        return 'Tempo de reserva de agendamento atualizado com sucesso!';
-      }
+      await this.scheduleTimeRepository.updateBySlotId(scheduleTimeRequest);
     } catch (e) {
       throw new DataBaseUpdateException();
     }
